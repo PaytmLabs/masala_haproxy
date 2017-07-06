6b78d8bcb5c5b245e291c1e238fa87b314ece75e
Joe Hohertz <jhohertz@gmail.com>
2017-07-06 12:50:49 -0400

    CHEF-127: rework haproxy cookbook to allow for 3rd interface (or possibly more).

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -16,24 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "masala_haproxy::default"
-
-node.default['masala_keepalived']['name'] = node['system']['short_hostname']
 node.default['masala_keepalived']['state'] = 'BACKUP'
 node.default['masala_keepalived']['priority'] = 100
-node.default['masala_keepalived']['advert_int'] = 1
-node.default['masala_keepalived']['nopreempt'] = false
-node.default['masala_keepalived']['auth_type'] = 'PASS'
-node.default['masala_keepalived']['auth_pass'] = node['masala_haproxy']['keepalive_password']
 
-node.default['masala_keepalived']['vi_1'] = node['masala_haproxy']['keepalive_vi_1']
-node.default['masala_keepalived']['vi_1']['track_script'] = 'chk_haproxy'
-
-# add second instance vi_2 config if specced, same as above for vi_1
-if ! node['masala_haproxy']['keepalive_vi_2'].nil?
-  node.default['masala_keepalived']['vi_2'] = node['masala_haproxy']['keepalive_vi_2']
-  node.default['masala_keepalived']['vi_2']['track_script'] = 'chk_haproxy'
-end
-
+include_recipe "masala_haproxy::default"
 include_recipe "masala_keepalived::default"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# common keepalive defaults
+
+node.default['masala_keepalived']['name'] = node['system']['short_hostname']
+node.default['masala_keepalived']['advert_int'] = 1
+node.default['masala_keepalived']['nopreempt'] = false
+node.default['masala_keepalived']['auth_type'] = 'PASS'
+node.default['masala_keepalived']['auth_pass'] = node['masala_haproxy']['keepalive_password']
+
+["vi_1", "vi_2", "vi_3"].each do |vi|
+  if ! node['masala_haproxy']["keepalive_#{vi}"].nil?
+    node.default['masala_keepalived'][vi] = node['masala_haproxy']["keepalive_#{vi}"]
+    node.default['masala_keepalived'][vi]['track_script'] = 'chk_haproxy'
+  else
+    if vi == "vi_1"
+      raise "keepalive_vi_1 must exist in configuration"
+    end
+  end
+end
+
+# haproxy defaults
 node.default['haproxy']['enable_default_http'] = false
 node.default['haproxy']['global_options'] = {
   'quiet' => ''
